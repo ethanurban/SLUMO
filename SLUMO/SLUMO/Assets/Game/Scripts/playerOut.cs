7e5a085041558b28514b3c149c28684802e3a23a
Ethan <buzzethanator@gmail.com>
2018-03-13 20:33:55 -0700

    almost ready for showcase
    -added "player out!" UI
    -added exit button when %player% wins (buttons still refusing to work)
    -Finally was able to make game music mute on "player win" screen with player win music!

---
+++ SLUMO/SLUMO/Assets/Game/Scripts/playerOut.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerOut : MonoBehaviour
+{
+    public GameObject p1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
+
+	
---
+++ SLUMO/SLUMO/Assets/Game/Scripts/quit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        Application.Quit();
+
+        Debug.Log("You Quit the Game!");
+	}
+}
